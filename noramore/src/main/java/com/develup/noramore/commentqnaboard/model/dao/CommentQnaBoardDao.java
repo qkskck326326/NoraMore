bc22eb0f4c8ba390e2912d9b0c067beb641f5645
WIN-DQN8OCBOPQS\ict01-10 <ksiwon520@gmail.com>
2024-04-09 15:02:23 +0900

    123

--- noramore/src/main/java/com/develup/noramore/commentqnaboard/model/dao/CommentQnaBoardDao.java
+++ noramore/src/main/java/com/develup/noramore/commentqnaboard/model/dao/CommentQnaBoardDao.java
@@ -1,5 +1,10 @@
 package com.develup.noramore.commentqnaboard.model.dao;
 
+import org.springframework.stereotype.Repository;
+
+@Repository("commentQnaBoardDao")
 public class CommentQnaBoardDao {
 
+	
+	
 }
