a71e529836c49a9c067fb9c1f1143dc8f636d735
WIN-DQN8OCBOPQS\ict01-10 <ksiwon520@gmail.com>
2024-04-09 17:16:22 +0900

    QNA 댓글관련 수정사항

--- noramore/src/main/java/com/develup/noramore/commentqnaboard/model/dao/CommentQnaBoardDao.java
+++ noramore/src/main/java/com/develup/noramore/commentqnaboard/model/dao/CommentQnaBoardDao.java
@@ -1,10 +1,42 @@
 package com.develup.noramore.commentqnaboard.model.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.develup.noramore.commentqnaboard.model.vo.CommentQnaBoard;
+import com.develup.noramore.commentrecrboard.model.vo.CommentRecrBoard;
 
 @Repository("commentQnaBoardDao")
 public class CommentQnaBoardDao {
 
+	@Autowired
+	SqlSessionTemplate sqlSessionTemplate;
 	
+	public int deleteComment(CommentQnaBoard commentQnaBoard) {
+		// TODO Auto-generated method stub
+		return sqlSessionTemplate.delete("qnacomment.deleteComment", commentQnaBoard);
+	}
+
+	public ArrayList<CommentQnaBoard> selectQnaComment(int boardId) {
+		List<CommentQnaBoard> list = sqlSessionTemplate.selectList("qnacomment.selectQnaComment", boardId);
+		return (ArrayList<CommentQnaBoard>)list;
+	}
+
+	public int updateComment(CommentQnaBoard commentQnaBoard) {
+		return sqlSessionTemplate.update("qnacomment.updateComment", commentQnaBoard);
+	}
+
+	public int insertQnaComment(CommentQnaBoard commentQnaBoard) {
+		// TODO Auto-generated method stub
+		return sqlSessionTemplate.insert("qnacomment.insertQnaComment", commentQnaBoard);
+	}
+
+	public void deleteBoardComment(int boardId) {
+		sqlSessionTemplate.delete("qnacomment.deleteBoardComment", boardId);
+	}	
 	
 }
--- noramore/src/main/java/com/develup/noramore/commentqnaboard/model/service/CommentQnaBoardService.java
+++ noramore/src/main/java/com/develup/noramore/commentqnaboard/model/service/CommentQnaBoardService.java
@@ -8,18 +8,10 @@ public interface CommentQnaBoardService {
 	int insertQnaComment(CommentQnaBoard commentQnaBoard);
 
 	ArrayList<CommentQnaBoard> selectQnaComment(int boardId);
-
-	void upcountcocoment(CommentQnaBoard commentQnaBoard);
-
-	void downcountcocoment(CommentQnaBoard commentQnaBoard);
-
-	ArrayList<CommentQnaBoard> selectQnaCocomment(CommentQnaBoard commentQnaBoard);
-
+		
 	int deleteComment(CommentQnaBoard commentQnaBoard);
 
 	int updateComment(CommentQnaBoard commentQnaBoard);
-
-	void deleteSubComment(CommentQnaBoard commentQnaBoard);
-
+	
 	void deleteBoardComment(int boardId);
 }
